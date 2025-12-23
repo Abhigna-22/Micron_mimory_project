21e86dbb6a7ab6e3c3a1aedc4119f59ad39d1293
Abhigna-22 <153512415+Abhigna-22@users.noreply.github.com>
2025-12-23 21:20:00 +0530

    Update memory.v

--- memory.v
+++ memory.v
@@ -10,11 +10,12 @@ reg [data_width-1:0] DRAM_block [0:(1<<width)-1]; //of data width is 8 then 1<<8
 always@(posedge clk or posedge reset)begin
     if(reset==1) data_out<={data_width{1'b0}};
 
-    else if(write_enb) begin DRAM_block[address]<=data_in; end
+  else if(write_enb && ! read_enb) begin DRAM_block[address]<=data_in; end
 
         //clock gating read operation
-    else if(read_enb)
+  else if(read_enb && !write_enb)
          data_out<=DRAM_block[address];
 
 end
+
 endmodule
